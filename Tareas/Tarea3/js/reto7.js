4a0b4aca77f9c8400a443b83d264d6ee5e9518eb
Roberto Rojas <robjimn@amazon.com>
2024-07-07 02:24:20 -0600

    Reto #7 - Completo

---
+++ Tareas/Tarea3/js/reto7.js
@@ -0,0 +1,102 @@
+$("#r1").click(function() {
+    Swal.fire({
+        icon: 'info',
+        title: 'Taj Mahal (India)',
+        showClass: {
+            popup: 'animated zoomIn',
+        },
+        hideClass: {
+            popup: 'animated zoomOut',
+        },
+        text: 'El Taj Mahal es un mausoleo de mármol blanco situado en Agra, India. Construido por el emperador mogol Shah Jahan en memoria de su esposa favorita, Mumtaz Mahal, el Taj Mahal es conocido por su belleza arquitectónica, su simetría y sus jardines. Es uno de los ejemplos más representativos del arte y la arquitectura mogol.',
+        footer: '<a href="https://en.wikipedia.org/wiki/Taj_Mahal" target="blank">Leer más</a>'
+    });
+});
+
+$("#r2").click(function() {
+    Swal.fire({
+        icon: 'info',
+        title: 'Gran Muralla China (China)',
+        showClass: {
+            popup: 'animated zoomIn',
+        },
+        hideClass: {
+            popup: 'animated zoomOut',
+        },
+        text: 'La Gran Muralla China es una serie de fortificaciones construidas a lo largo de las fronteras históricas del norte de China para proteger y consolidar los territorios chinos de las invasiones y ataques de los nómadas eurasiáticos. Extendiéndose por miles de kilómetros, es un testimonio de la determinación y la ingeniería de las dinastías chinas.',
+        footer: '<a href="https://en.wikipedia.org/wiki/Great_Wall_of_China" target="blank">Leer más</a>'
+    });
+});
+
+$("#r3").click(function() {
+    Swal.fire({
+        icon: 'info',
+        title: 'Machu Picchu (Perú)',
+        showClass: {
+            popup: 'animated zoomIn',
+        },
+        hideClass: {
+            popup: 'animated zoomOut',
+        },
+        text: 'Machu Picchu es una ciudadela inca situada en las montañas de los Andes en Perú, sobre el valle del río Urubamba. Construida en el siglo XV y redescubierta en 1911 por Hiram Bingham, Machu Picchu es conocida por su sofisticada arquitectura, sus terrazas agrícolas y sus impresionantes vistas panorámicas.',
+        footer: '<a href="https://en.wikipedia.org/wiki/Machu_Picchu" target="blank">Leer más</a>'
+    });
+});
+
+$("#r4").click(function() {
+    Swal.fire({
+        icon: 'info',
+        title: 'Petra (Jordania)',
+        showClass: {
+            popup: 'animated zoomIn',
+        },
+        hideClass: {
+            popup: 'animated zoomOut',
+        },
+        text: 'Petra es una histórica ciudad nabatea esculpida en la roca, situada en el sur de Jordania. Fundada en el siglo IV a.C., Petra es famosa por sus monumentos tallados en la piedra, como el tesoro de Al-Khazneh, y por su avanzada ingeniería hidráulica que permitió prosperar en un entorno desértico.',
+        footer: '<a href="https://en.wikipedia.org/wiki/Petra" target="blank">Leer más</a>'
+    });
+});
+
+$("#r5").click(function() {
+    Swal.fire({
+        icon: 'info',
+        title: 'Cristo Redentor (Brasil)',
+        showClass: {
+            popup: 'animated zoomIn',
+        },
+        hideClass: {
+            popup: 'animated zoomOut',
+        },
+        text: 'El Cristo Redentor es una icónica estatua de Jesucristo situada en la cima del cerro del Corcovado en Río de Janeiro, Brasil. Esta colosal estatua de 30 metros de altura, inaugurada en 1931, se ha convertido en un símbolo de la ciudad y del país, representando la paz y la hospitalidad del pueblo brasileño.',
+        footer: '<a href="https://en.wikipedia.org/wiki/Christ_the_Redeemer_(statue)" target="blank">Leer más</a>'
+    });
+});
+$("#r6").click(function() {
+    Swal.fire({
+        icon: 'info',
+        title: 'Coliseo Romano (Italia)',
+        showClass: {
+            popup: 'animated zoomIn',
+        },
+        hideClass: {
+            popup: 'animated zoomOut',
+        },
+        text: 'El Coliseo Romano es un anfiteatro ovalado en el centro de Roma, Italia. Inaugurado en el año 80 d.C., es una de las mayores hazañas de la ingeniería y la arquitectura romanas. Con capacidad para decenas de miles de espectadores, el Coliseo albergaba eventos de gladiadores, caza de animales y espectáculos públicos.',
+        footer: '<a href="https://en.wikipedia.org/wiki/Colosseum" target="blank">Leer más</a>'
+    });
+});
+$("#r7").click(function() {
+    Swal.fire({
+        icon: 'info',
+        title: 'Chichén Itzá (México)',
+        showClass: {
+            popup: 'animated zoomIn',
+        },
+        hideClass: {
+            popup: 'animated zoomOut',
+        },
+        text: 'Chichén Itzá es una antigua ciudad maya ubicada en la península de Yucatán, México. Es famosa por su pirámide principal, El Castillo, también conocida como el Templo de Kukulcán. Este sitio arqueológico muestra la avanzada comprensión de los mayas sobre la astronomía y la arquitectura, y es uno de los sitios más visitados de México.',
+        footer: '<a href="https://en.wikipedia.org/wiki/Chichen_Itza" target="blank">Leer más</a>'
+    });
+});
